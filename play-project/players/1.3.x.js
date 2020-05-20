7d8164877b2bd47d4988a73d5c94a4b5dd6655d5
AE-Hopscotch <98666048+AE-Hopscotch@users.noreply.github.com>
2020-05-19 23:19:40 -0700

    Fix the sound thing

--- play-project/players/1.3.x.js
+++ play-project/players/1.3.x.js
@@ -2804,8 +2804,8 @@ console.log("Webplayer v1.3.9 - 2020/05/13 (production)");
 				function notePath (val) {
 					//AE_MOD find path of note
 					var isCustom = (!/^((low-|high)?[a-zA-Z](sharp|flat)?|clickPlayable|alert|car|chaChing|check|clang|crash|dash|doorbell|drip|fail|footsteps|laser|pop|schoolBell|spring|vibrate|trophy|aliens|bubbles|crickets|meow|rain|roar|tweet|wind|woof|ahhh|cheer|eating|heartbeat|laugh|news|talking|bass|chord|clap|gong|snare)$/.test(val));
-					var i = (/^(low-|high)?[a-zA-Z](sharp|flat)?$/.test(val)) ? ({"-1": "", "0": "new/", "1": "guitar/", "2": "8-bit/"})[(c[2])?c[2].computedStringValue(b):'0'] : ((isCustom)?"custom/":"");
-					return (!isCustom && !!getPref && getPref("old_sounds")) ? "" : i;
+					var ins = (/^(low-|high)?[a-zA-Z](sharp|flat)?$/.test(val)) ? ({"-1": "", "0": "new/", "1": "guitar/", "2": "8-bit/"})[(i[2])?i[2].computedStringValue(e):'0'] : ((isCustom)?"custom/":"");
+					return (!isCustom && !!getPref && getPref("old_sounds")) ? "" : ins;
 				}
                 var r = o.HSSoundManager.sharedInstance//, s = i[0].computedStringValue(e);
 				var s = notePath(i[0].computedStringValue(e)) + i[0].computedStringValue(e);
