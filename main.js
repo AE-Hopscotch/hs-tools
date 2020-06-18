c275187f82186a864b0ecbe9f98a6cfe6709a69e
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2020-06-18 16:24:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ function addSiteVisit(branch) {
 }
 
 var lastPageExceptions = /https:\/\/awesome-e\.github\.io\/hs-tools\/?($|play-project|explore-channel\/about)/;
-if (!lastPageExceptions.test(window.location.href)) setCookie('lastPage', window.location.href, 30);
+if (!lastPageExceptions.test(window.location.href) && self === top) setCookie('lastPage', window.location.href, 30);
 if (getCookie('visitedWithinHour') != 'true' && /https:\/\/awesome-e.github.io\/hs-tools\//gi.test(location.href)) {
 	if (window.location.href != "https://awesome-e.github.io/hs-tools/") addSiteVisit(location.href.replace(/.*?hs-tools\//,''));
 	addSiteVisit();
