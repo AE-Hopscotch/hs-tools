ed674b24638bb23ff808675ae7e8b6e6ffab6c6a
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2020-07-08 13:40:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-var server = app.listen(8080, function() {
-    console.log('Ready on port %d', server.address().port);
-});
+let http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8124, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:8124/');
